0320e1d5fcc23a34d825b29f452c23c32d7097a0
Sean Brandenburg <seanbrandenburg333@gmail.com>
2023-06-29 16:34:42 -0400

    add auth and user store

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import * as Realm from "realm-web";
+  import { goto } from '$app/navigation';
+  import { browser } from '$app/environment'; 
+  import { user } from '$lib/stores'
+
+  if (browser){
+    Realm.handleAuthRedirect()
+    goto('/')
+  }
+</script>
+
